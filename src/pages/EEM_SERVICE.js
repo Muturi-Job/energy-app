91fd68d6e12e0bdfb44ffdad610566e8a3e005e7
Job Muturi <muirimuturi17@gmail.com>
2023-06-26 15:31:11 +0300

    Updated EEM Service Data

---
+++ src/pages/EEM_SERVICE.js
@@ -0,0 +1,79 @@
+import React from "react"
+
+function EEM_SERVICE() {
+
+    return (
+        <>
+            <div className="container container-md page-title ">
+                <h2>Energy Efficiency and Management Services</h2>
+            </div>
+
+            <div className=" container container-md page-intro">
+                <p>Sarufi aims to empower organizations to become more energy-efficient, sustainable, and resilient by delivering innovative solutions and comprehensive capacity building programs. We strive to create a greener future by optimizing energy consumption, reducing greenhouse gas emissions, and fostering a culture of environmental stewardship</p>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Energy Audit & Assessment</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Conducting comprehensive energy audits to identify areas of energy waste and inefficiency.</li>
+                    <li>Performing detailed energy assessments to develop customized recommendations for improvement.</li>
+                    <li>Providing cost-benefit analysis to help organizations make informed decisions about energy-saving measures.</li>
+                </ul>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Energy Management Systems</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Developing and implementing robust energy management systems to monitor and control energy usage.</li>
+                    <li>Installing advanced metering and monitoring systems for real-time energy consumption data.</li>
+                    <li>Creating energy dashboards and reporting tools for tracking performance and identifying optimization opportunities.</li>
+                </ul>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Energy Efficiency Solutions</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Designing and implementing energy-efficient technologies and solutions tailored to specific industry needs.</li>
+                    <li>Providing energy-efficient lighting, HVAC systems, and equipment upgrades.</li>
+                    <li>Introducing renewable energy solutions such as solar panels and wind turbines to offset energy consumption.</li>
+                </ul>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Training & Capacity Building</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Conducting workshops and training programs to educate employees on energy conservation practices.</li>
+                    <li>Developing customized capacity building programs to enhance organizational energy management capabilities.</li>
+                    <li>Providing guidance on regulatory compliance, energy policies, and sustainability certifications.</li>
+                </ul>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Energy Audit, Monitoring & Reporting</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Monitoring energy performance, analyzing data, and providing regular progress reports.</li>
+                    <li>Identifying trends, benchmarking performance, and recommending further improvements.</li>
+                    <li>Assisting with energy performance certification, such as LEED or ENERGY STAR.</li>
+                </ul>
+            </div>
+            <div className="container container-md services-header">
+                <h3>Sustainability Consulting</h3>
+            </div>
+            <div className="container container-md services-content">
+                <ul>
+                    <li>Offering strategic guidance on sustainability initiatives and goals.</li>
+                    <li>Assisting in the development of sustainability action plans and carbon footprint reduction strategies.</li>
+                    <li>Conducting life cycle assessments to identify environmental hotspots and develop mitigation strategies.</li>
+                </ul>
+            </div>
+
+        </>
+    );
+
+}
+export default EEM_SERVICE
