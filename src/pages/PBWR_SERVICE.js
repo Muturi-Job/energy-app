863260d997492a185ebf24d2fdd7e661e73472f9
Job Muturi <muirimuturi17@gmail.com>
2023-06-27 08:30:45 +0300

    Updated data for PBWR_SERVICE

---
+++ src/pages/PBWR_SERVICE.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+function PBWR_SERVICE() {
+    return (
+        <>
+            <div className="container container-md page-title ">
+                <h2>PET Bottle Waste Recycling</h2>
+            </div>
+            <div className=" container container-md page-intro">
+                <p>PET bottles, which make up 25% of dumpsite space, have an incredibly slow decomposition rate of 500 to 1000 years. Furthermore, Africa and Asia together contribute 90% of the world's ocean plastic waste, while Africa, despite producing only 6% of global plastic, imports 70% of the plastic consumed, highlighting the region's struggle with circularity and mismanagement of plastic waste.</p>
+            </div>
+
+            <section className="container need-for-PET-recycling-section">
+                <div className="container container-md section-heading">
+                    <h2>The Need For PET Bottle Recycling</h2>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Environmental Impact</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>PET (Polyethylene terephthalate) bottles are made
+                        from non-renewable fossil fuels and take hundreds
+                        of years to decompose if not properly recycled.
+                        Improper disposal of PET bottles contributes to
+                        pollution of land, water bodies, and marine
+                        ecosystems. Recycling these bottles reduces the
+                        need for new plastic production and helps conserve
+                        resources.</p>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Reduction of Energy Consumption</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>Recycling PET bottles significantly reduces energy
+                        consumption compared to producing new bottles
+                        from raw materials. The recycling process requires
+                        less energy, as the collected bottles are melted
+                        down and reprocessed into new products. This helps
+                        to conserve energy resources and reduce
+                        greenhouse gas emissions associated with
+                        manufacturing processes.</p>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Conservation of Natural Resources</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>Recycling PET bottles allows for the recovery and
+                        reuse of valuable resources, such as petroleum and
+                        natural gas. By recycling, we can reduce the
+                        extraction and consumption of these limited
+                        resources, preserving them for future generations.
+                        Additionally, recycling PET bottles reduces the
+                        demand for other raw materials used in bottle
+                        production, such as water and minerals.</p>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Waste Reduction</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>PET bottles make up a significant portion of
+                        municipal solid waste worldwide. By recycling these
+                        bottles, we can divert them from landfills and
+                        incinerators, reducing the volume of waste and the
+                        associated environmental and health impacts.
+                        Recycling also helps to alleviate the strain on waste
+                        management systems and promotes a more
+                        sustainable waste management approach.</p>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Economic Benefits</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>PET bottle recycling can contribute to the creation of
+                        a circular economy by generating employment
+                        opportunities and supporting local industries. The
+                        recycling industry creates jobs in collection, sorting,
+                        processing, and manufacturing of recycled PET
+                        materials. Additionally, recycled PET can be used to
+                        produce various products, such as new bottles,
+                        textiles, packaging materials, and more, creating a
+                        market for recycled materials.</p>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Public Awareness & Education</h3>
+                </div>
+                <div className="container container-md services-content">
+                    <p>Promoting PET bottle recycling raises awareness
+                        about the environmental impact of plastic waste
+                        and encourages individuals, businesses, and
+                        communities to adopt sustainable practices.
+                        Education campaigns and initiatives can help people
+                        understand the importance of recycling, proper
+                        waste management, and the benefits of choosing
+                        reusable alternatives.</p>
+                </div>
+
+            </section>
+            <section className="container integrated-plant-section">
+            <div className="container container-md section-heading">
+                    <h2>The Sarufi Intergrated Plant</h2>
+            </div>
+            <div className=" container container-md page-intro">
+                <p>Our integrated PET bottle waste recycling system aims to maximize the value of PET bottle waste, minimize waste generation, conserve resources, and reduce the environmental impact associated with PET bottle production and disposal, by creating a sustainable and circular economy where PET bottles are recycled, reused, and transformed into valuable products, contributing to a greener future.</p>
+            </div>
+            </section>
+
+            <section className="container PET-recycling-value-chain-section">
+                <div className="container container-md section-heading">
+                    <h2>The Need For PET Bottle Recycling</h2>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>PET Flakes</h3>
+                </div>
+                <div className="container container-md services-content">
+                 <p>PET flakes can be used in various applications, including:</p>
+                 <ul>
+                    <li><span className="fw-bold">Polyester Staple Fiber:</span>PET flakes are transformed into different types of polyester staple fiber, such as solid, regular hollow conjugated fibers.</li>
+                    <li><span className="fw-bold">PET Film:</span>PET flakes are processed into PET film, which finds application in various industries.</li>
+                    <li><span className="fw-bold">PET Strap:</span>PET flakes are converted into PET straps,which are widely used for packaging purposes.</li>
+
+                 </ul>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Polyester Staple Fiber</h3>
+                </div>
+                <div className="container container-md services-content">
+                 <p>Polyester staple fiber derived from PET flakes has diverse uses, including:</p>
+                 <ul>
+                    <li><span className="fw-bold">Home Furnishing & Toy Stuffing:</span>It is employed in home furnishing items and toy stuffing.</li>
+                    <li><span className="fw-bold">Mattress, Pillows and Bedding Products:</span>Polyester staple fiber is utilized in the production of mattresses,pillows and Bedding products</li>
+                    <li><span className="fw-bold">Non-Wooven Textile:</span>It serves as a crucial component in non-wooven textile manufacturing.</li>
+                    <li><span className="fw-bold">Wadding & Batting Products:</span>Polyester staple fiber is used in the production of wadding and batting products.</li>
+                    <li><span className="fw-bold">Padding Material:</span>It finds application as padding material in various industries.</li>
+
+                 </ul>
+                </div>
+                <div className="container container-md services-header">
+                    <h3>Non-Wooven Geotextile</h3>
+                </div>
+                <div className="container container-md services-content">
+                 <p>Non-woven geotextile, made from polyester staple fiber, has multiple applications, including:</p>
+                 <ul>
+                    <li><span className="fw-bold">Geotextile:</span>Non-woven geotextile is used for soil stabilization and erosion control in civil engineering projects.</li>
+                    <li><span className="fw-bold">Acoustic Insulation:</span> It provides sound insulation in buildings and automotive applications.</li>
+                    <li><span className="fw-bold">Padding Product:</span>Non-woven geotextile serves as padding material in different industries.</li>
+                    <li><span className="fw-bold">Carpet Underlay:</span> It is utilized as an underlay material for carpets.</li>
+                    <li><span className="fw-bold">Thermal Insulation:</span>Non-woven geotextile acts as thermal insulation in construction and insulation applications.</li>
+
+                 </ul>
+                </div>
+
+            </section>
+
+        </>
+    );
+
+}
+
+export default PBWR_SERVICE;
